df7252c8caac9435c15c628d6029a02b83ab5fb7
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-02-09 11:41:03 +0200

    Add quest card

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,30 +1,33 @@
 import { FaSearch } from "react-icons/fa";
-import "./HeroSection.scss";
+import "../styles/HeroSection.scss";
 
 function HeroSection() {
   return (
-    <section className="hero">
-      <div className="hero__content">
-        <h1 className="hero__title">
-          PITCH YOUR STARTUP, <br /> CONNECT WITH ENTREPRENEURS
-        </h1>
-        <p className="hero__subtitle">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
-        </p>
+    <>
+      <section className="hero">
+        <div className="hero__content">
+          <h1 className="hero__title">
+            PITCH YOUR STARTUP, <br /> CONNECT WITH ENTREPRENEURS
+          </h1>
+          <p className="hero__subtitle">
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+            Competitions.
+          </p>
 
-        <div className="hero__search">
-          <input
-            type="text"
-            placeholder="Search Startups"
-            className="hero__search-input"
-          />
-          <button className="hero__search-button">
-            <FaSearch />
-          </button>
+          <div className="hero__search">
+            <input
+              type="text"
+              placeholder="Search Startups"
+              className="hero__search-input"
+            />
+            <button className="hero__search-button">
+              <FaSearch />
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
-};
+}
 
-export default HeroSection
+export default HeroSection;

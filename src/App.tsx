bb0ed0c33b1eb8edc7d564b30880b22ec548f1b2
ariia horunzha <1mpellee@gmail.com>
2025-02-09 13:08:52 +0200

    Created profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.scss'
 import { Navbar } from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import Profile from './components/Profile'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-        <Navbar></Navbar>
-        <HeroSection></HeroSection>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,37 @@
+import "./Profile.scss";
+
+function Profile() {
+  return (
+    <section className="profile">
+      <h2 className="profile__text">My profile</h2>
+      <div className="profile__block">
+        <img
+          className="profile__photo"
+          src="src\assets\profile_photo_default.png"
+          alt="Profile"
+        />
+        <div className="profile__info">
+        <img
+              className="profile__rating_stars"
+              src="src\assets\profile_rating_stars.webp"
+            />
+            <h3 className="profile_name">Name Surname</h3>
+          <p className="profile__biography">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque ligula odio, ultricies eget vestibulum et, euismod a
+              enim. Vestibulum efficitur nibh leo, at auctor nisi dictum quis.
+              Morbi aliquet feugiat arcu sed ultricies. Quisque eleifend vitae
+              ligula sit amet rutrum. Vivamus magna ante, dapibus quis mollis sit
+              amet, interdum non nisi.
+            </p>
+        </div>
+      </div>
+      <h2 className="profile__text">All Name's quests:</h2>
+      <div className="profile__block">
+        <h6>Quests</h6>
+      </div>
+    </section>
+  );
+}
+
+export default Profile;

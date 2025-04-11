f01a47059fae3da8192e1a172199a79113d01461
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-04-11 12:48:21 +0300

    Fix app.tsx error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Partial/Navbar";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import QuestForm from "./components/Forms/QuestForm";
-import QuestionsForm from "./components/Forms/QuestionForm";
 import SignUp from "./components/Login/SignUp";
 import LogIn from "./components/Login/LogIn";
 import Footer from "./components/Partial/Footer";
@@ -20,7 +19,6 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/quest-form/1" element={<QuestForm />} />
-        <Route path="/quest-form/2" element={<QuestionsForm />} />
         <Route path="/rating-form" element={<RatingForm />} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/log-in" element={<LogIn/>} />

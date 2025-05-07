7a1f0a7e6b2d7a70ee84f27df4253fb1bf600c4e
Maks9m <maxim.kramarenko8a@gmail.com>
2025-05-07 18:05:01 +0300

    refactor: create constants folder

---
+++ src/constants/questConstants.ts
@@ -0,0 +1,14 @@
+const CATEGORIES: string[] = [
+    "All",
+    "Adventure",
+    "Puzzle",
+    "Educational",
+    "Gaming",
+    "Team challenges",
+    "Mystery",
+    "Other",
+  ];
+
+const QUESTS_URL = "http://localhost:5000/quests";
+
+export { CATEGORIES, QUESTS_URL };

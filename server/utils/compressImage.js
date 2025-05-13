db2552061c2c996d1c39584fe8885ab72ee05dbd
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-05-13 15:01:02 +0300

    fix: fix large delay before creating the quest

--- server/utils/compressImage.js
+++ server/utils/compressImage.js
@@ -2,10 +2,11 @@ import sharp from "sharp";
 
 export const compressImage = async (base64String) => {
   if (!base64String) return "";
-  const matches = base64String.match(/^data:.+\/(.+);base64,(.*)$/);
-  const buffer = matches
-    ? Buffer.from(matches[2], "base64")
-    : Buffer.from(base64String, "base64");
+
+  const idx = base64String.indexOf(',');
+  const base64Data = idx !== -1 ? base64String.slice(idx + 1) : base64String;
+
+  const buffer = Buffer.from(base64Data, "base64");
 
   const resizedBuffer = await sharp(buffer)
     .resize(null, 500, { fit: "cover" })
@@ -13,5 +14,7 @@ export const compressImage = async (base64String) => {
     .jpeg({ quality: 70 })
     .toBuffer();
 
-  return `data:image/jpeg;base64,${resizedBuffer.toString("base64")}`;
+  const result = `data:image/jpeg;base64,${resizedBuffer.toString("base64")}`;
+
+  return result;
 };

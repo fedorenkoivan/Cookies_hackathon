b08f8bd0c357eb322a3268b4e30d9ff6308f438b
fedorenkoivan <ivan.r.fedorenko@gmail.com>
2025-05-24 22:55:27 +0300

    delete old sendEmail func

--- server/utils/authUtils.js
+++ server/utils/authUtils.js
@@ -57,27 +57,6 @@ export const validateCredentails = async (credentials) => {
   };
 };
 
-export const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  });
-
-  const mailOptions = {
-    from: 'Cookies Reset Password <noreply@cookiesquest.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    html: options.html
-  };
-
-  await transporter.sendMail(mailOptions);
-};
-
 export const createAccessToken = (userId) => {
   const idStr = userId.toString();
   return jwt.sign(

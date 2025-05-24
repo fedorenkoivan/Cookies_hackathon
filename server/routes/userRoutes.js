3645acdbd0ca9de5654f8765a5cac7362503922f
Impe11e <1mpellee@gmail.com>
2025-05-24 11:12:03 +0300

    refactor: the profile controller logic moved to routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,10 +8,10 @@ import { sendEmail } from "../utils/sendEmailProxy.js";
 
 import argon2 from "argon2";
 
-import { getProfile } from "../controllers/userController.js";
 import { verifyToken } from "../middleware/authMiddleWare.js";
 import { logRoute } from "../middleware/loggerMiddleware.js";
 import { User } from "../models/userModel.js";
+import { createError } from '../utils/errorUtils.js';
 
 
 export default async function userRoutes(fastify) {
@@ -95,9 +95,26 @@ export default async function userRoutes(fastify) {
       },
     },
     async (request, reply) => {
-      return getProfile(request, reply);
-    },
-  );
+      const userId = request.user.id;
+      
+      const user = await User.findById(userId);
+      
+      if (!user) {
+        throw createError(404, 'User not found');
+      }
+
+      return reply.code(200).send({
+        status: 'success',
+        data: {
+          user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
+          }
+        }
+      })
+    }
+  );  
 
 
   fastify.post("/forgot-password", { preHandler: logRoute("forgot_password") },

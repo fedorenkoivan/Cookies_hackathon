9022690ebd9d012b14efb2f88103e32cc41aef6c
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-05-26 18:37:22 +0300

    feat: add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader-message">Loading...</div>
+      <div className="loader">
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -8,6 +8,7 @@ import QuestCard from "../Home/QuestCard";
 import "@/components/Home/QuestCard.scss";
 import { FaEdit, FaShareAlt } from "react-icons/fa";
 // import { BidirectionalPriorityQueue } from "@/utils/BidirectionalPriorityQueue";
+import Loading from "../Loading/Loading";
 interface UserData {
   id: string;
   name: string;
@@ -148,7 +149,7 @@ const Profile = () => {
   // }, []);
 
   if (loading) {
-    return <div className="profile__container">Loading...</div>;
+    return <Loading />;
   }
 
   if (error) {
